36cc342c3ef88c7141c8c8a28b1b1541b4ad143d
Zubenko1 <zubenkodp@gmail.com>
2022-11-16 18:42:44 +0200

    .

--- src/pages/DashBorderPage.jsx
+++ src/pages/DashBorderPage.jsx
@@ -1,17 +1,4 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import { useLogOutRedirect } from 'hooks/useLogOutRedirect';
-
-// const useLogOutRedirect = () => {
-//   const navigate = useNavigate();
-//   const isLoggenIn = useSelector(state => state.user.isLoggenIn);
-//   useEffect(() => {
-//     if (!isLoggenIn) {
-//       navigate('/login', { replace: true });
-//     }
-//   }, [isLoggenIn, navigate]);
-// };
 
 export const DashBorderPage = () => {
   useLogOutRedirect();
